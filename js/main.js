c5912201b4cf81af6f183735c789b9a5a43a549f
ffabioladel <al243453@edu.uaa.mx>
2023-03-03 00:02:32 -0600

    No mucho meh

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ function colocarAnimales(animales) {
         //Imprime imagenes
         imagenes.innerHTML += `
             <div style="position: absolute; left: ${pleft}px; top: ${ptop}px;">
-                <img src="images/${animales[i]}.png" style="width: 100px;" alt="">
+                <img src="images/${animales[i]}.png" style="width: 160px;" alt="">
             </div>
         `;
         pleft += 200;
--- js/sonido.js
+++ js/sonido.js
@@ -3,16 +3,19 @@ let sound = true;
 let MiAudio = document.getElementById("Audio-2");
 
 function ChangeImgOn(){
-   document.querySelector(".Musi-Off").src="../images/delfin.pngf";
+	document.querySelector(".Musi-Off").src="../images/No_Music.svg";
 }
 MiAudio.addEventListener('ended', ChangeImgOn);
 
 function Sonido(){
-   if(sound){
-      document.querySelector(".Musi-Off").src="../images/cangrejo.png";
-      MiAudio.src="../sounds/fondo.wav";	
-      MiAudio.volume = 0.9;
-      MiAudio.play();	
-   }
+	if(sound){
+		document.querySelector(".Musi-Off").src="../images/Si_Music.svg";
+		MiAudio.src="../sounds/fondo.wav";	
+		MiAudio.volume = 0.7;
+		MiAudio.play();	
+	}else{
+        MiAudio.stop();
+    }
 
 }
+
